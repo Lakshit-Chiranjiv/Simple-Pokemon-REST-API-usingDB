a539d344f50bbe442ce7e74987acd0a416be1fbc
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-03-18 17:32:38 +0530

    db connection done

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import { MONGO_DB_URI } from './dbConfig.js';
 const app = express();
+
+mongoose.connect(MONGO_DB_URI)
+.then(()=>{console.log("Connected to database")})
+.catch((err) => {console.log(err)});
+
+
 
 app.get('/',(req,res)=>{
     res.send("hwllovv");
 })
 
-app.listen(8000,()=>{console.log("server running")});
+app.listen(8000,()=>{console.log("server running on port 8000")});
