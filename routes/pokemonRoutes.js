9dbef9f2d559a49c6f15fcb9d49357e1c9745b1f
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-03-19 16:48:38 +0530

    contoller setup for get random pokemon route

--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -6,6 +6,7 @@ import { dirname } from 'path';
 import { getAllPokemons } from "../controllers/getAllPokemons.js";
 import { getAPokemon } from "../controllers/getAPokemon.js";
 import { getTypePokemons } from "../controllers/getTypePokemons.js";
+import { getRandomPokemon } from "../controllers/getRandomPokemon.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -31,16 +32,7 @@ router.get('/getOne/:id',getAPokemon);
 
 router.get('/getByType/:type',getTypePokemons);
 
-router.get('/getRandom',async(req,res)=>{
-    try {
-        const allPokemon = await pokemonModel.find();
-        if(!allPokemon) throw Error('could not find pokemon');
-        const randomPokemon = allPokemon[Math.floor(Math.random() * allPokemon.length)];
-        res.status(200).json(randomPokemon);
-    } catch (error) {
-        res.status(400).json({message: error});
-    }
-});
+router.get('/getRandom',getRandomPokemon);
 
 router.post('/add',async(req,res)=>{
     req.body.name = capitalizeWholeString(req.body.name);
