7155ac7633e5053347b2ff577adf92525c12e392
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-03-19 10:28:20 +0530

    get all pokemons route added

--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -7,6 +7,17 @@ router.get('/',(req,res)=>{
     res.send('<h1>Pokemons API</h1>');
 });
 
+router.get('/getAll',async(req,res)=>{
+    try {
+        const allPokemons = await pokemonModel.find();
+        if(!allPokemons) throw Error('cannot get due to some server error');
+        res.status(200).json(allPokemons);
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+});
+
+
 
 
 
